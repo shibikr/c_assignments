3e5120f52c4a1319a2f8fc6ddac7a03d2f6626a4
shibik <shibik@INshibik.local>
2016-01-04 17:33:02 +0530

    [shibi] - added a function for create array
    What changes have you made?
    	created function and struct
    *
    
    Why do we need this change?
    	to create resizable array
    *
    
    What can this change impact?
    
    *

--- assignment1/main.c
+++ assignment1/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <assert.h>
-#include <string.h>
+#include <stdlib.h>
 #include "assignment1.h"
 
-int main(){
+ArrayUtil create(int typeSize, int length){
+	ArrayUtil *base = (ArrayUtil *)calloc(length,typeSize);
+	return *base;
+};
 
+int main(){
+	printf("%d\n",create(4,10));
+	return 0;
 };
